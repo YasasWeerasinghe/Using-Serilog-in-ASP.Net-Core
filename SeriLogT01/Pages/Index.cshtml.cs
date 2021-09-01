4e65532e3ff710e21964238f665e082eac237002
Yasas <yasas.2015041@iit.ac.lk>
2021-09-01 17:58:56 +0530

    Serilog

---
+++ SeriLogT01/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLogT01.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("****Test 01 logger in index page");
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i == 56)
+                    {
+                        throw new Exception("this is a demo exception");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("The value of i is {LoopCountValue}", i);
+                    }
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "** rex log error");
+            }
+        }
+    }
+}
